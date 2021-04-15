70d355b2c0a1ca30aae58508687062afa53bb52f
Christopher Kane <christopher.kane@stonybrook.edu>
2021-04-14 22:46:51 -0400

    Created template for index.html, added index() function to views.py to render index.html when user visits the base page for learning logs.

---
+++ learning_logs/urls.py
@@ -0,0 +1,9 @@
+"""Defines URL patterns for learning_logs."""
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+url_patterns = [
+    # Home page
+    path('', views.index, name = 'index'),
+]
