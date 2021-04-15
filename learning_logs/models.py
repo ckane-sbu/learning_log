ecfe946c2379b45acc8ff6f836edc8a43706f5a2
Christopher Kane <christopher.kane@stonybrook.edu>
2021-04-14 22:13:54 -0400

    Added the Entry model and registered it with the admin site.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,3 +10,19 @@ class Topic(models.Model):
         """Return a string representation of the Topic Model."""
         return self.text
 
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of an Entry object."""
+        if len(self.text) <= 50:
+            return self.text
+        else:
+            return f"{self.text[:50]}..."
+        
