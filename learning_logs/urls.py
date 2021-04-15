5a8ac152d010a279bc37597dd8f6b9aa1af9645f
Christopher Kane <christopher.kane@stonybrook.edu>
2021-04-14 23:24:14 -0400

    Added URL, view, and template for individual topics pages. CHAPTER 18 COMPLETE

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
-url_patterns = [
+urlpatterns = [
     # Home page
     path('', views.index, name = 'index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name = 'topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
 ]
